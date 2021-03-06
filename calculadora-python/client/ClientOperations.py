c84eee0120f0a1bb908260b17bb1a2b3b2f2c59b
Cristhian <c.camilosky1120@gmail.com>
2021-03-06 11:06:49 -0500

    Archivos

---
+++ calculadora-python/client/ClientOperations.py
@@ -0,0 +1,1 @@
+from server.Operation import Operations
---
+++ calculadora-python/server/Operation.py
@@ -0,0 +1,7 @@
+class Operations:
+    def __init__ (self,numberOne,numberTwo):
+        self.numberOne = numberOne
+        self.numberTwo = numberTwo
+
+    def Sum(self, numberOne,numberTwo):
+        return self.numberOne + self.numberTwo
