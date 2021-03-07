ac0d39f6fad4ea47bb21e0ae6de19a5374ff9652
Diego Capera <41840919+diegocapera95@users.noreply.github.com>
2021-03-07 17:05:43 -0500

    Cambios varios, se incluye cliente y servidor del chat

--- calculadora-python/server/Operation.py
+++
@@ -1,8 +0,0 @@
-
-class Operation:
-    def __init__ (self,numberOne,numberTwo):
-        self.numberOne = numberOne
-        self.numberTwo = numberTwo
-
-    def Sum(self, numberOne,numberTwo):
-        return self.numberOne + self.numberTwo
